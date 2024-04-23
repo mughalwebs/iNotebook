b7ad43ed3e396f0c15cbeda8a1fbe3e60d05d6bc
Mughalsahab <144914285+Mughalsahab@users.noreply.github.com>
2024-04-23 11:49:02 +0500

    Add validation

--- Backend/index.js
+++ Backend/index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json())
 app.use('/api/auth', require('./routes/authentication'))
 // app.get('/api/notes', require('./routes/notes'))
 
--- Backend/routes/authentication.js
+++ Backend/routes/authentication.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
+const { body, validationResult } = require('express-validator')
 
-router.get('/', (request, response) => {
-    let obj = {
-        name: 'Hamid Raza',
-        age: 16
+router.post('/', [
+    body('email', 'Enter a valid email address').isEmail(),
+    body('name').isLength({ min: 3 }),
+    body('password').isLength({ min: 8 })
+], (request, response) => {
+    const error = validationResult(request);
+    if (!error.isEmpty()) {
+        return response.status(400).json({ error: error.array() })
     }
-    response.json(obj);
+    User.create({
+        name: request.body.name,
+        email: request.body.email,
+        password: request.body.password,
+    }).then((user) => { response.send("Data Stored Successfully.") })
+        .catch((err) => {
+            response.json({error: 'Please Enter Valid Informations', message: err.msg})
+        })
 })
 
 module.exports = router;

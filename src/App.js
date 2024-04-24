5107a27fb50acb33cb3fcf0ff7290228f5fb41ba
Mughalsahab <144914285+Mughalsahab@users.noreply.github.com>
2024-04-24 14:12:50 +0500

    Create a Navbar and make different Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './Components/Navbar.';
+import Home from './Components/Home';
+import About from './Components/About';
 
 function App() {
   return (
     <>
-      <h1>iNotebook Pending</h1>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/about' element={<About />} />
+        </Routes>
+      </Router>
     </>
   );
 }
